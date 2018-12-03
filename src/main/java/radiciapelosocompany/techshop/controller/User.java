2f517f0c4ba6293bca1e420e9fcea0f00cb8c0e1
Mauro Peloso <mauropeloso96@gmail.com>
2018-12-03 17:51:27 +0100

    Continue first commit.

--- src/main/java/radiciapelosocompany/techshop/controller/User.java
+++ src/main/java/radiciapelosocompany/techshop/controller/User.java
@@ -1,5 +1,47 @@
 package radiciapelosocompany.techshop.controller;
 
 public class User {
+	private String nome;
+	private String cognome;
+	private String username;
+	private String password;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [nome=" + nome + ", cognome=" + cognome + ", username=" + username + ", password=" + password
+				+ "]";
+	}
 
 }
