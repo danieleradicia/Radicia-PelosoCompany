610956fa9e7f70490ac3417fd0f30b5922e2f4b0
Daniele Radicia <danieleradiciat@gmail.com>
2018-12-03 15:59:57 +0100

    Start TechShop for Radicia&PelosoCompany

---
+++ src/main/java/radiciapelosocompany/techshop/App.java
@@ -0,0 +1,13 @@
+package radiciapelosocompany.techshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
